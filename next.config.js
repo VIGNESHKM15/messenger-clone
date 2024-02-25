771fbe43797aa3e7bed5be26f9604819160f326b
VIGNESHKM15 <69468034+VIGNESHKM15@users.noreply.github.com>
2024-02-26 01:06:47 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/**
+ *   {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    swcPlugins: [["next-superjson-plugin", {}]]
+  },
+  image: {
+    domains: [
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com"
+    ]
+  }
+}
+
+module.exports =  {
+  reactStrictMode: true,
+  experimental: {
+      forceSwcTransforms: true,
+  },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+        },
+      ],
+    },
+}
